d2f28e976fbeb56c202b85aa69a5c0713b97eca2
Ekua Dennis <ebdennis@usc.edu>
2024-11-21 23:12:23 -0500

    Added basic functionality

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -6,6 +6,8 @@ export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
   const [loading, setLoading] = useState(false);
   const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -27,9 +29,36 @@ export default function GlobalState({ children }) {
     }
   }
 
+  function handleAddToFavorites(currentItem) {
+    let favoritesListCopy = [...favoritesList];
+    //check if item is already present in fav items list
+    const index = favoritesListCopy.findIndex(
+      (item) => item.id === currentItem.id
+    );
+    if (index === -1) {
+      favoritesListCopy.push(currentItem);
+    }
+    //if item is already present, remove form favorites
+    else {
+      favoritesListCopy.splice(index);
+    }
+
+    setFavoritesList(favoritesListCopy);
+  }
+
   return (
     <GlobalContext.Provider
-      value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit }}
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        handleAddToFavorites,
+        favoritesList,
+      }}
     >
       {children}
     </GlobalContext.Provider>
